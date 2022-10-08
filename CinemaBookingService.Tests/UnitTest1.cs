a51311fcd772f7bab5e80121afbf4863efc0579a
jay2bid <jaybidokwu@gmail.com>
2022-10-08 14:56:00 +0100

    Initial commit with empty classes and tests

---
+++ CinemaBookingService.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CinemaBookingService.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
